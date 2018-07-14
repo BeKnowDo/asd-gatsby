00841a045db2317bc30b72768ee81c3ab3e256ba
Cesar Perez <cesar@bkd.io>
2018-07-14 00:42:18 -0400

    added toggle to hide/show navigation items

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,45 +1,57 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import Link from 'gatsby-link'
 import IoCloseRound from 'react-icons/lib/io/close-round'
 import TiThMenu from 'react-icons/lib/ti/th-menu'
 import { Sc_Navigation, Sc_Menu, Sc_CloseMenu } from './styles'
 
-const Navigation = () => {
-  return (
-    <Sc_Navigation>
-      <Sc_Menu>
-        <TiThMenu />
-      </Sc_Menu>
-      <Sc_CloseMenu>
-        <IoCloseRound />
-      </Sc_CloseMenu>
-      <nav>
-        <ul>
-          <li>
-            <Link>Home</Link>
-          </li>
-          <li>
-            <Link>Features</Link>
-          </li>
-          <li>
-            <Link>Pricing &amp; Sign-up</Link>
-          </li>
-          <li>
-            <Link>Our Mission</Link>
-          </li>
-          <li>
-            <Link>Testimonials</Link>
-          </li>
-          <li>
-            <Link>Login</Link>
-          </li>
-          <li>
-            <Link>Blog</Link>
-          </li>
-        </ul>
-      </nav>
-    </Sc_Navigation>
-  )
+class Navigation extends PureComponent {
+  constructor() {
+    super()
+    this.state = {
+      nav_open: false,
+    }
+  }
+
+  toggleNavigation = () => {
+    this.setState({ nav_open: !this.state.nav_open })
+  }
+  render() {
+    return (
+      <Sc_Navigation {...this.state}>
+        <Sc_Menu {...this.state} onClick={this.toggleNavigation}>
+          <TiThMenu size="28" />
+        </Sc_Menu>
+        <Sc_CloseMenu {...this.state} onClick={this.toggleNavigation}>
+          <IoCloseRound size="28" />
+        </Sc_CloseMenu>
+        <nav>
+          <ul>
+            <li>
+              <Link>Home</Link>
+            </li>
+            <li>
+              <Link>Features</Link>
+            </li>
+            <li>
+              <Link>Pricing &amp; Sign-up</Link>
+            </li>
+            <li>
+              <Link>Our Mission</Link>
+            </li>
+            <li>
+              <Link>Testimonials</Link>
+            </li>
+            <li>
+              <Link>Login</Link>
+            </li>
+            <li>
+              <Link>Blog</Link>
+            </li>
+          </ul>
+        </nav>
+      </Sc_Navigation>
+    )
+  }
 }
 
 export default Navigation
--- src/components/navigation/styles.js
+++ src/components/navigation/styles.js
@@ -6,25 +6,31 @@ const Sc_Navigation = styled.div`
   color: ${colors.black};
 
   nav {
-    display: none;
+    display: ${props => (props.nav_open === true ? 'block' : 'none')};
+    padding: 16px 20px 10px;
   }
 
   a {
     display: inline-block;
-    padding: 10px;
+    font-size: 1.4rem;
+    padding: 14px;
   }
 `
 
 const Sc_Menu = styled.span`
+  display: ${props => (props.nav_open === false ? 'block' : 'none')};
   position: absolute;
-  top: 20px;
+  top: 24px;
   right: 20px;
+  cursor: pointer;
 `
 
 const Sc_CloseMenu = styled.span`
+  display: ${props => (props.nav_open === true ? 'block' : 'none')};
   position: absolute;
-  top: 20px;
+  top: 24px;
   right: 20px;
+  cursor: pointer;
 `
 
 export { Sc_Navigation, Sc_Menu, Sc_CloseMenu }
